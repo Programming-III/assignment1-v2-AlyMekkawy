cc7d1b06efa142ee8f7ff84723650ded9c16df5a
Aly Moataz <aly.elmekawy@student.giu-uni.de>
2025-11-11 09:42:37 +0200

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -5,7 +5,16 @@
 using namespace std;
 //#write Instructor class here
 
+class Instructor : public Person{
+private:
+    string department;
+    int experienceYears;
 
+public:
+    void display() override;
+    Instructor(string n, int id, string dept, int year);
+    ~Instructor();
+};
 
 
 
