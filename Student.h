8edfeaaf20f465d36313a2203bd558e89be786fe
Aly Moataz <aly.elmekawy@student.giu-uni.de>
2025-11-11 09:42:55 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -3,10 +3,22 @@
 
 #include "Person.h"
 #include <string>
-#using namespace std;
+using namespace std;
 //#write student class here
 
+class Student : public Person{
+private:
+    int yearLevel;
+    string major;
 
+public:
+    //override keyword just to ensure correct behavior, looks nice
+    void display() override;
+
+
+    Student(string n, int id, int y, string m);
+    ~Student();
+};
 
 
 
