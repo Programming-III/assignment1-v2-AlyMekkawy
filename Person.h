f9c8cfe7f0710194aef32dbc96fda1205fbedffc
Aly Moataz <aly.elmekawy@student.giu-uni.de>
2025-11-11 09:43:11 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -2,10 +2,24 @@
 #define PERSON_H
 
 #include <string>
+#include <iostream>
 using namespace std;
 
 //#write Person class here
+class Person{
+private:
+    string name;
+    int id;
 
+public:
+    virtual void display();
+
+    string getName();
+    int getID();
+
+    Person(string name, int id);
+    ~Person();
+};
 
 
 
