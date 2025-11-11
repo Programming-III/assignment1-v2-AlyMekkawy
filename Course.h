b2c97235ff291f2a6ed3a9c86f0e732fadc98ff5
Aly Moataz <aly.elmekawy@student.giu-uni.de>
2025-11-11 09:42:18 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -6,6 +6,22 @@
 using namespace std;
 //#write your code here
 
+class Course{
+private:
+    string courseCode, courseName;
+    int maxStudents, currentStudents;
+    Student* students;
+
+public:
+    Course(string courseCode, string courseName, int maxStud);
+
+    ~Course();
+
+    bool addStudent(const Student& s);
+
+    void displayCourseInfo();
+
+};
 
 
 
